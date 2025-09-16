97d0f358e0f4cd63a5dd5131fb4604518cc92fd7
andychen313 <andyc313@nycstudents.net>
2025-09-16 09:24:27 -0400

    Create DataTypes.py

---
+++ DataTypes.py
@@ -0,0 +1,40 @@
+""" x = 3
+y = float(3)
+print(x, y) """
+
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values[0])
+print(values[6])
+values.append("32")
+print(values[7]) """
+
+
+""" x = "this is a thing"
+y = x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+#Create an input function to ask the user for a sentence (inputs output strings).
+sentence = input("Give me a sentence")
+y = sentence.split( )
+""" print(len(x)) """
+
+""" day_of_week = input("What day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = "test"
+print(f"hello {x}") """
+
+""" temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
+ """
